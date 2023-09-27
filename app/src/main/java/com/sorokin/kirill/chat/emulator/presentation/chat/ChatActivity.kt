cb63e43eb8aa80f5d6392dd27b615985d454728f
llerik <aztec8688@yandex.ru>
2023-09-27 15:11:04 +0200

    Привязал логику к экрану

--- app/src/main/java/com/sorokin/kirill/chat/emulator/presentation/chat/ChatActivity.kt
+++ app/src/main/java/com/sorokin/kirill/chat/emulator/presentation/chat/ChatActivity.kt
@@ -7,15 +7,19 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.getSystemService
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textfield.TextInputLayout
 import com.sorokin.kirill.chat.emulator.R
 import com.sorokin.kirill.chat.emulator.R.layout
+import com.sorokin.kirill.chat.emulator.domain.models.MessageModel
 import com.sorokin.kirill.chat.emulator.presentation.chat.adapter.MessagesAdapter
+import com.sorokin.kirill.chat.emulator.presentation.core.App
 
 class ChatActivity : AppCompatActivity() {
 	private lateinit var textInputField: TextInputLayout
+	private lateinit var viewModel: ChatViewModel
 	private val messagesAdapter = MessagesAdapter()
 
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,17 +45,38 @@ class ChatActivity : AppCompatActivity() {
 					if (text.isNotBlank()) {
 						Log.d(TAG, "onCreate: button click: $text")
 						it.clear()
-
+						viewModel.sendMessage(text)
 						hideKeyboard(textInputField)
 					}
 				}
 				buttonIsEnable = true
 			}
 		}
+
+		viewModel = ViewModelProvider(
+			viewModelStore, (application as App).component.chatViewModelFactory
+		)[ChatViewModel::class.java]
+		viewModel.init()
+		viewModel.getMessagesLiveData().observe(this, ::updateMessages)
+	}
+
+	override fun onStart() {
+		super.onStart()
+		viewModel.start()
+	}
+
+	override fun onStop() {
+		super.onStop()
+		viewModel.stop()
 	}
 
 	private fun hideKeyboard(view: View) {
 		getSystemService<InputMethodManager>()?.hideSoftInputFromWindow(view.windowToken, 0)
+	}
+
+	private fun updateMessages(messages: List<MessageModel>) {
+		Log.d(TAG, "updateMessages: $messages")
+		messagesAdapter.submitList(messages)
 	}
 
 	companion object {
